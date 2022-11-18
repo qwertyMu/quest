5905662269cdc462edd58779eb44e406202136a8
OperationXen <OperationXen@apocryphasecurity.com>
2022-11-18 16:15:10 +0000

    Added wordcloud (typescript)

--- src/components/AttributionWordCloud.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function AttributionWordCloud() {
-  return null;
-}
--- src/components_v2/showResults.js
+++ src/components_v2/showResults.js
@@ -6,7 +6,7 @@ import { Box } from "@mui/material";
 import PropTypes from "prop-types";
 import { GET_ATTRIBUTIONS, GET_INTERACTIONS } from "../queries";
 import TabbedResults from "./tabbedResults";
-import AttributionWordCloud from "../components/AttributionWordCloud";
+import AttributionWordCloud from "../components/AttributionWordCloud.tsx";
 import AttributionsCard from "./AttributionsCard";
 import InteractionsCard from "./interactionsCard";
 
