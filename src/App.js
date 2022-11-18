e2f3c30db08f0f81038d3e92ff9754f2413950a9
OperationXen <OperationXen@apocryphasecurity.com>
2022-11-18 12:42:04 +0000

    Tidied imports

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React, { Component } from "react";
-import "./App.css";
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import AnalysisContainer from "./components/analysisContainer";
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context';
-import MyIntelDrawer from "./components/myIntelDrawer";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { Authenticator } from "@aws-amplify/ui-react";
-import '@aws-amplify/ui-react/styles.css';
 
+import { styled } from "@mui/material/styles";
+import { Paper } from "@mui/material";
+
+import AnalysisContainer from "./components/analysisContainer";
+import MyIntelDrawer from "./components/myIntelDrawer";
+
+import "@aws-amplify/ui-react/styles.css";
+import "./App.css";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#f5f2f2',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#f5f2f2",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
 const httpLink = createHttpLink({
-  uri: 'https://75pakaytozcsbgl2niax4vgsjy.appsync-api.eu-west-2.amazonaws.com/graphql',
+  uri: "https://75pakaytozcsbgl2niax4vgsjy.appsync-api.eu-west-2.amazonaws.com/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
   // const token = localStorage.getItem('token');
-  const token = 'da2-mif62xdc2ncxpl7hunwoyvgmji';
+  const token = "da2-mif62xdc2ncxpl7hunwoyvgmji";
   // return the headers to the context so httpLink can read them
   return {
     headers: {
@@ -33,22 +36,22 @@ const authLink = setContext((_, { headers }) => {
       // authorization: token ? `${token}` : "",
       authorization: token ? `${token}` : "",
       "x-api-key": token ? `${token}` : "",
-    }
-  }
+    },
+  };
 });
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
-
 class App extends Component {
-
-  
   render() {
     return (
-      <div className="App" style={{height: '100%', backgroundColor: "#1a1b21" }}>
+      <div
+        className="App"
+        style={{ height: "100%", backgroundColor: "#1a1b21" }}
+      >
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <Authenticator>
           <ApolloProvider client={client}>
