9bc7e4eda51a7be4a805033e5a55db589b6a9c75
OperationXen <OperationXen@apocryphasecurity.com>
2022-11-26 23:16:26 +0000

    Simplified intel drawer and added zustand control

---
+++ src/components/IntelDrawer.js
@@ -0,0 +1,105 @@
+import React from "react";
+
+import { Global } from "@emotion/react";
+import { styled } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { grey } from "@mui/material/colors";
+import { Box, Typography, SwipeableDrawer } from "@mui/material";
+
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+import interfaceState from "../datastore/interfaceStore.tsx";
+import TabbedIntelligenceList from "./tabbedIntelligencelist";
+
+const drawerBleeding = 56;
+
+const Root = styled("div")(({ theme }) => ({
+  height: "100%",
+  backgroundColor:
+    theme.palette.mode === "light"
+      ? grey[100]
+      : theme.palette.background.default,
+}));
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "light" ? "#fff" : grey[800],
+}));
+
+const Puller = styled(Box)(({ theme }) => ({
+  width: 30,
+  height: 6,
+  backgroundColor: theme.palette.mode === "light" ? grey[300] : grey[900],
+  borderRadius: 3,
+  position: "absolute",
+  top: 8,
+  left: "calc(50% - 15px)",
+}));
+
+export default function IntelDrawer(props) {
+  const [open, setOpen] = interfaceState((s) => [s.drawerOpen, s.setDrawOpen]);
+  const { window } = props;
+
+  // This is used only for the example
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Root>
+      <CssBaseline />
+      <Global
+        styles={{
+          ".MuiDrawer-root > .MuiPaper-root": {
+            height: `calc(80% - ${drawerBleeding}px)`,
+            overflow: "visible",
+          },
+        }}
+      />
+      <Box sx={{ textAlign: "center", pt: 1, backgroundColor: "#1a1b21" }}>
+        <KeyboardArrowDownIcon
+          sx={{ color: "#f05c54" }}
+          fontSize="large"
+          onClick={() => setOpen(!open)}
+        />
+      </Box>
+      <SwipeableDrawer
+        container={container}
+        anchor="bottom"
+        open={open}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        swipeAreaWidth={drawerBleeding}
+        disableSwipeToOpen={false}
+        ModalProps={{
+          keepMounted: true,
+        }}
+      >
+        <StyledBox
+          sx={{
+            position: "absolute",
+            top: -drawerBleeding,
+            borderTopLeftRadius: 8,
+            borderTopRightRadius: 8,
+            visibility: "visible",
+            right: 0,
+            left: 0,
+          }}
+        >
+          <Puller />
+          <Typography sx={{ p: 2, color: "text.secondary" }}>
+            My Intel Drawer
+          </Typography>
+        </StyledBox>
+        <StyledBox
+          sx={{
+            px: 2,
+            pb: 2,
+            height: "100%",
+            overflow: "auto",
+          }}
+        >
+          <TabbedIntelligenceList />
+        </StyledBox>
+      </SwipeableDrawer>
+    </Root>
+  );
+}
