fe6e64b3271fad132a4ee01e6bc2ab4497a7d8fe
OperationXen <OperationXen@apocryphasecurity.com>
2022-12-01 21:41:30 +0000

    Added slide transition to top level entities
    Removed scaling for top navmenu for now

---
+++ src/components/navigation/NavigationContainer.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+import { useLocation } from "react-router-dom";
+import { Box, Slide } from "@mui/material";
+
+import RelationshipMap from "../graph/RelationshipMap";
+import GenericSearch from "../search/GenericSearch";
+
+export default function NavigationContainer() {
+  const location = useLocation();
+  const [component, setComponent] = useState<string>("");
+
+  useEffect(() => {
+    if (location.pathname === "/network") setComponent("network");
+    else setComponent("search");
+  }, [location.pathname, setComponent]);
+
+  return (
+    <React.Fragment>
+      <Slide direction="right" in={component === "search"}>
+        <Box>
+          <GenericSearch />
+        </Box>
+      </Slide>
+      <Slide direction="left" in={component === "network"}>
+        <Box>
+          <RelationshipMap />
+        </Box>
+      </Slide>
+    </React.Fragment>
+  );
+}
