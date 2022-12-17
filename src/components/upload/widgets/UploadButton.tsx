e1ca79edcc8af4b63cbbe17d582a0c2070d81c6f
OperationXen <OperationXen@apocryphasecurity.com>
2022-12-17 17:03:35 +0000

    Default to first element in list

--- src/components/upload/widgets/UploadButton.tsx
+++ src/components/upload/widgets/UploadButton.tsx
@@ -14,7 +14,7 @@ const options = [
 export default function UploadButton() {
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef<HTMLDivElement>(null);
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
 
   const handleClick = () => {
     console.info(`You clicked ${options[selectedIndex]}`);
