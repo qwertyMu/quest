f81b26d9720acba84f0489fd3fb0c71ea3c924a8
OperationXen <OperationXen@apocryphasecurity.com>
2022-12-18 15:14:44 +0000

    Added some QoL changes

--- src/components/search/widgets/AttributionDisplaySelect.tsx
+++ src/components/search/widgets/AttributionDisplaySelect.tsx
@@ -1,6 +1,7 @@
 import React, { useState, cloneElement } from "react";
 
-import { Popper, Box, Tooltip } from "@mui/material";
+import { Popper, Box, Typography, Tooltip } from "@mui/material";
+import { Zoom, ClickAwayListener } from "@mui/material";
 
 import SmartphoneIcon from "@mui/icons-material/Smartphone";
 import CloudIcon from "@mui/icons-material/Cloud";
@@ -38,36 +39,71 @@ export default function AttributionDisplaySelect(props: PropTypes) {
 
   return (
     <React.Fragment>
-      <Tooltip title={getTooltipText()} placement="right">
+      <Tooltip title={getTooltipText()} placement="right" enterDelay={1500}>
         {cloneElement(getCurrentSelectionIcon(), {
           sx: { color: "white", cursor: "pointer" },
           onClick: handleClick,
         })}
       </Tooltip>
+
       <Popper
         id="attribution-display-select"
         open={Boolean(anchorEl)}
         anchorEl={anchorEl}
-        placement="bottom"
+        placement="bottom-start"
       >
-        <Box
-          sx={{
-            marginLeft: "4px",
-            display: "flex",
-            padding: "4px",
-            background: "darkgrey",
-            borderRadius: "8px",
-          }}
-        >
-          <CloudIcon
-            sx={{ cursor: "pointer" }}
-            onClick={() => handleChange("wordcloud")}
-          />
-          <SmartphoneIcon
-            sx={{ cursor: "pointer" }}
-            onClick={() => handleChange("cardlist")}
-          />
-        </Box>
+        <ClickAwayListener onClickAway={() => setAnchorEl(null)}>
+          <Zoom in={true}>
+            <Box
+              sx={{
+                marginLeft: "-4px",
+                marginTop: "-2em",
+                display: "flex",
+                flexDirection: "column",
+                padding: "8px",
+                background: "darkgrey",
+                borderRadius: "8px",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  gap: "8px",
+                  alignItems: "center",
+                  cursor: "pointer",
+                }}
+                onClick={() => handleChange("wordcloud")}
+              >
+                <CloudIcon sx={{ cursor: "pointer" }} />
+                <Typography
+                  sx={{
+                    fontFamily: "monospace",
+                  }}
+                >
+                  Word cloud
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  gap: "8px",
+                  alignItems: "center",
+                  cursor: "pointer",
+                }}
+                onClick={() => handleChange("cardlist")}
+              >
+                <SmartphoneIcon sx={{ cursor: "pointer" }} />
+                <Typography
+                  sx={{
+                    fontFamily: "monospace",
+                  }}
+                >
+                  Group by source
+                </Typography>
+              </Box>
+            </Box>
+          </Zoom>
+        </ClickAwayListener>
       </Popper>
     </React.Fragment>
   );
